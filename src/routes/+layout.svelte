333c9631f457ddf35ce6502c0b5aae5ff6d9b905
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-08-30 21:01:05 +0530

    Setup Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input, Footer, FooterCopyright } from 'flowbite-svelte';
+  	import { SearchOutline } from 'flowbite-svelte-icons';
+</script>
+
+<header class="border-2">
+	<Navbar>
+		<NavBrand href="/">
+			<span class="self-center whitespace-nowrap text-xl font-bold dark:text-white mr-2">
+				Voter's Data -
+			</span>
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+				Owe, Kharghar
+			</span>
+		</NavBrand>
+		<div class="flex md:order-2">
+			<Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+			<SearchOutline class="w-5 h-5" />
+			</Button>
+			<div class="hidden relative md:block">
+			<div class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none">
+				<SearchOutline class="w-4 h-4" />
+			</div>
+			<Input id="search-navbar" class="ps-10" placeholder="Search..." />
+			</div>
+			<NavHamburger />
+		</div>
+		<!-- <NavUl>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/docs/components/navbar">Navbar</NavLi>
+		</NavUl> -->
+	</Navbar>
+</header>
+
+<main style="min-height: 81.5vh;">
+	<slot/>
+</main>
+
+<footer class="border-2">
+	<Navbar>
+		<Footer rounded color="form" class="w-full flex items-center text-center justify-center">
+			<FooterCopyright href="/" by="Voter's Data™" year={2024} />
+		</Footer>
+	</Navbar>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { 
+        Card,
+        Button,
+        Input,
+        Label,
+        Hr,
+    } from "flowbite-svelte";
+
+    let username = "Sajid Patel";
+    let password = "";
+    let key="1234"
+    var login = false;
+    var error = "";
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if(password==key){
+            login = true;
+            error = "";
+        }else{
+            error = "Incorrect Password";
+        }
+    }
+
+    import Home from './home.svelte';
+    
+</script>
+
+
+{#if login}
+    <Home />
+{:else}
+    <div class="py-10 px-72">
+        <h1 class="text-3xl font-semibold text-gray-900 dark:text-white text-center pb-10">
+            Welcome to Voter's Data Portal
+        </h1>
+        <Card class="mx-auto max-w-full">
+            <form class="flex flex-col space-y-6" action="/">
+                <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                    Login to Access Data
+                </h3>
+                
+                <Hr classHr="my-8" />
+                <Label class="space-y-2">
+                    <span>Full Name</span>
+                    <Input type="text" name="name" placeholder="John" bind:value={username} required />
+                </Label>
+                
+                <Label class="space-y-2">
+                    <span>Your password</span>
+                    <Input type="password" name="password" placeholder="•••••" bind:value={password} required />
+                </Label>
+                {#if error.length>0}
+                    <p class="text-red-500">{error}</p>
+                {/if}
+                <Label>
+                    <Button on:click={handleSubmit} class="w-48">
+                        Login
+                    </Button>
+                </Label>
+            </form>
+        </Card>
+    </div>
+{/if}
