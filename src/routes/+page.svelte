611528a001643ba2074b2cf87d25d992dbc17cf7
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-09-10 10:47:59 +0530

    Added OTP base authentication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,88 @@
 <script>
-	import { 
-        Card,
-        Button,
-        Input,
-        Label,
-        Hr,
-    } from "flowbite-svelte";
+	import { Card, Button, Input, Label, Hr, Alert, Spinner } from 'flowbite-svelte';
 
-    let username = "Sajid Patel";
-    let password = "";
-    let key="1234"
-    var login = false;
-    var error = "";
+	let username = 'Sajid Patel';
+	let verificationCode = Math.floor(1000 + Math.random() * 9000).toString();
+	var error = '';
+	var alert = '';
+	let password = '';
+	let sendingCode = false;
+	let alertColor = 'green';
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        if(password==key){
-            login = true;
-            error = "";
-            window.location.href = "/data";
-        }else{
-            error = "Incorrect Password";
-        }
-    }
-    
+	async function sendVerificationEmail() {
+		sendingCode = true;
+		try {
+			const response = await fetch(`/api/send-otp`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					verificationCode
+				})
+			});
+
+			const result = await response.json();
+
+			if (result.success) {
+				alert = 'Verification email sent successfully!';
+				sendingCode = false;
+			} else {
+				alert = 'Failed to send verification email.';
+				alertColor = 'red';
+			}
+		} catch (error) {
+			console.error('Error sending verification email:', error);
+		}
+	}
+
+	function handleSubmit() {
+		if (password == verificationCode) {
+			window.location.href = '/data';
+		} else {
+			error = 'Incorrect Verification Code';
+		}
+	}
 </script>
 
 <div class="py-10 md:px-72">
-    <h1 class="text-3xl font-semibold text-gray-900 dark:text-white text-center pb-10">
-        Welcome to Voter's Data Portal
-    </h1>
-    <Card class="max-w-full">
-        <form class="flex flex-col space-y-6" action="/">
-            <h3 class="text-xl font-medium text-gray-900 dark:text-white">
-                Login to Access Data
-            </h3>
-            
-            <Hr classHr="my-8" />
-            <Label class="space-y-2">
-                <span>Full Name</span>
-                <Input type="text" name="name" placeholder="John" bind:value={username} required />
-            </Label>
-            
-            <Label class="space-y-2">
-                <span>Your password</span>
-                <Input type="password" name="password" placeholder="•••••" bind:value={password} required />
-            </Label>
-            {#if error.length>0}
-                <p class="text-red-500">{error}</p>
-            {/if}
-            <Label>
-                <Button on:click={handleSubmit} class="w-48">
-                    Login
-                </Button>
-            </Label>
-        </form>
-    </Card>
+	<h1 class="pb-10 text-center text-3xl font-semibold text-gray-900 dark:text-white">
+		Welcome to Voter's Data Portal
+	</h1>
+	<Card class="max-w-full">
+		<form class="flex flex-col space-y-6" action="/">
+			{#if alert}
+				<Alert color={alertColor} class="text-green-900">{alert}</Alert>
+			{/if}
+			{#if error}
+				<Alert class="text-red-800">{error}</Alert>
+			{/if}
+			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Login to Access Data</h3>
+			<Hr classHr="my-8" />
+			<Label class="space-y-2">
+				<span>Full Name</span>
+				<Input type="text" name="name" placeholder="John" bind:value={username} disabled />
+			</Label>
+			<Label class="space-y-2">
+				<span>Enter Verification Code</span>
+				<Input type="password" name="password" placeholder="•••••" bind:value={password} required />
+			</Label>
+			<div class="grid grid-cols-2 gap-3">
+				{#if alert === 'Verification email sent successfully!'}
+					<Label>
+						<Button on:click={handleSubmit} class="w-full">Login to Portal</Button>
+					</Label>
+				{:else if sendingCode}
+					<Button disabled class="w-full">
+						<Spinner class="me-3" size="4" color="white" />
+						Sending Verification Code
+					</Button>
+				{:else}
+					<Label>
+						<Button on:click={sendVerificationEmail} class="w-full">Send Verification Code</Button>
+					</Label>
+				{/if}
+			</div>
+		</form>
+	</Card>
 </div>
---
+++ src/routes/api/send-otp/+server.js
@@ -0,0 +1,37 @@
+import { env } from '$env/dynamic/private';
+import nodemailer from 'nodemailer';
+
+// Create a mail transporter using nodemailer for Outlook
+const transporter = nodemailer.createTransport({
+	host: 'smtp.office365.com',
+	port: 587,
+	secure: false, // Use TLS
+	auth: {
+		user: 'csmvschatbot@outlook.com',
+		pass: env.EMAIL_PASS
+	},
+	tls: {
+		ciphers: 'SSLv3' // Add this to avoid some TLS-related errors
+	}
+});
+
+export async function POST({ request }) {
+	const { verificationCode } = await request.json();
+
+	try {
+		// Define email options
+		const mailOptions = {
+			from: 'csmvschatbot@outlook.com',
+			to: 'abdurrahman19702@gmail.com',
+			subject: "Your OTP to acess Voter's Data",
+			text: `Enter the following verification code to access our portal:\n\n${verificationCode}`
+		};
+
+		await transporter.sendMail(mailOptions);
+
+		return new Response(JSON.stringify({ success: true }), { status: 200 });
+	} catch (error) {
+		console.error('Error sending email:', error);
+		return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
+	}
+}
