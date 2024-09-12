5eddee700563ac5626441670f418a40e20bebe91
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-09-12 09:40:21 +0530

    Removed cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			out: 'build'
+		})
 	},
 	preprocess: vitePreprocess()
 };
