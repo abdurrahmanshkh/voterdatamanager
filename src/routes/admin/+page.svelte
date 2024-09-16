1b8c031676db742b0781a7ba3bf7dd19fe68d367
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-09-16 14:35:14 +0530

    Added delete surveyor and messaging options

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,9 +1,10 @@
 <script>
-	import { Button, Input, Label, Card, P } from 'flowbite-svelte';
+	import { Button, Input, Label, Card, P, Alert } from 'flowbite-svelte';
 	import { Table, TableBody, TableBodyCell, Modal } from 'flowbite-svelte';
 	import { TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { ExclamationCircleOutline } from 'flowbite-svelte-icons';
 
 	onMount(() => {
 		if (!localStorage.getItem('isAuthenticated')) {
@@ -14,6 +15,7 @@
 	let alert = '';
 	let name = '';
 	let phoneNo = '';
+	let popupModal = false;
 
 	// Function to handle form submission
 	async function handleSubmit(event) {
@@ -52,10 +54,53 @@
 
 	export let data; // The fetched data is passed as props to the page component
 	let { surveyors } = data; // Destructure the surveyors from the data prop
+
+	// Function to delete a surveyor
+	async function deleteSurveyor(id) {
+		try {
+			const response = await fetch(`/api/delete-surveyor/${id}`, {
+				method: 'POST'
+			});
+
+			if (response.ok) {
+				const result = await response.json();
+				alert = 'Surveyor deleted successfully!';
+				//Refresh page to show new surveyor
+				location.reload();
+			} else {
+				alert = 'Failed to delete surveyor';
+			}
+		} catch (error) {
+			alert = 'An error occurred';
+		}
+	}
+
+	// Function to create the SMS link
+	function sendSMS(name, phoneNo, link) {
+		let message = `Dear ${name},\n\nYou have been successfully onboarded as a surveyor for the voter survey in your designated area.\n\nYour details are as follows:\n\n- Name: ${name}\n- Phone No: ${phoneNo}\n- Access Link: ${link}\n\nPlease use the access link to log in and start your survey. If you have any questions, feel free to contact us.\n\nThank you for your valuable contribution!`;
+
+		// Encode the message to be used in the SMS link
+		let smsLink = `sms:+91${phoneNo}?&body=${encodeURIComponent(message)}`;
+		// Redirect to the SMS app
+		window.location.href = smsLink;
+	}
+
+	// Function to create the WhatsApp link
+	function sendWhatsAppMessage(name, phoneNo, link) {
+		let message = `Dear ${name},\n\nYou have been successfully onboarded as a surveyor for the voter survey in your designated area.\n\nYour details are as follows:\n\n- Name: ${name}\n- Phone No: ${phoneNo}\n- Access Link: ${link}\n\nPlease use the access link to log in and start your survey. If you have any questions, feel free to contact us.\n\nThank you for your valuable contribution!`;
+
+		// Encode the message to be used in the WhatsApp link
+		let whatsappLink = `https://wa.me/+91${phoneNo}?text=${encodeURIComponent(message)}`;
+		// Open WhatsApp with the pre-filled message
+		window.location.href = whatsappLink;
+	}
 </script>
 
 <main>
 	<Card class="mx-auto max-w-full border-2 border-gray-300 bg-gray-100">
+		{#if alert}
+			<Alert color="green" class="text-bold mt-4 text-green-950">{alert}</Alert>
+		{/if}
 		<P class="mb-4 text-xl font-bold">Add Surveyor</P>
 		<form on:submit|preventDefault={handleSubmit}>
 			<div class="grid gap-4 md:grid-cols-3">
@@ -71,9 +116,6 @@
 			</div>
 		</form>
 	</Card>
-	{#if alert}
-		<P class="mt-4 text-center text-red-500">{alert}</P>
-	{/if}
 	<Card class="mx-auto max-w-full border-2 border-gray-300 bg-gray-100">
 		<div class="grid md:grid-cols-3">
 			<P class="mb-4 text-xl font-bold md:col-span-2">Current Surveyors</P>
@@ -84,17 +126,55 @@
 				<TableHeadCell>Phone No</TableHeadCell>
 				<TableHeadCell>Forms Filled</TableHeadCell>
 				<TableHeadCell>Forms Updated</TableHeadCell>
+				<TableHeadCell>Send Details</TableHeadCell>
+				<TableHeadCell>Delete</TableHeadCell>
 			</TableHead>
 			<TableBody>
 				{#each surveyors as surveyor}
-					<TableBodyRow
-						class="border-blue-900 bg-blue-100 hover:bg-blue-200"
-						on:click={() => goto(`/surveyors/${surveyor._id}`)}
-					>
+					<TableBodyRow class="border-blue-900 bg-blue-100 hover:bg-blue-200">
 						<TableBodyCell>{surveyor.name}</TableBodyCell>
 						<TableBodyCell>{surveyor.phoneNo}</TableBodyCell>
 						<TableBodyCell>{surveyor.formsFilled}</TableBodyCell>
 						<TableBodyCell>{surveyor.formsUpdated}</TableBodyCell>
+						<TableBodyCell class="">
+							<Button
+								color="blue"
+								on:click={() =>
+									sendSMS(
+										surveyor.name,
+										surveyor.phoneNo,
+										`https://sajidpatel.pages.dev/${surveyor._id}`
+									)}
+								class="mr-2 w-28"
+								>SMS
+							</Button>
+							<Button
+								color="green"
+								on:click={() =>
+									sendWhatsAppMessage(
+										surveyor.name,
+										surveyor.phoneNo,
+										`https://sajidpatel.pages.dev/${surveyor._id}`
+									)}
+								class="w-28"
+								>WhatsApp
+							</Button>
+						</TableBodyCell>
+						<TableBodyCell>
+							<Button color="red" on:click={() => (popupModal = true)}>Delete</Button>
+						</TableBodyCell>
+						<Modal bind:open={popupModal} autoclose>
+							<div class="text-center">
+								<ExclamationCircleOutline class="mx-auto mb-4 h-12 w-12 text-gray-900" />
+								<h3 class="mb-5 text-lg font-normal text-gray-900">
+									Are you sure you want to delete this surveyor?
+								</h3>
+								<Button on:click={() => deleteSurveyor(surveyor._id)} color="red" class="me-2"
+									>Yes, I'm sure</Button
+								>
+								<Button color="alternative">No, cancel</Button>
+							</div>
+						</Modal>
 					</TableBodyRow>
 				{/each}
 			</TableBody>
---
+++ src/routes/api/delete-surveyor/[surveyorId]/+server.js
@@ -0,0 +1,33 @@
+import { env } from '$env/dynamic/private';
+
+export async function POST({ params }) {
+	const apiKey = env.API_KEY;
+	const endpoint =
+		'https://ap-south-1.aws.data.mongodb-api.com/app/data-mxiiynz/endpoint/data/v1/action/deleteOne';
+
+	try {
+		const response = await fetch(endpoint, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'api-key': apiKey
+			},
+			body: JSON.stringify({
+				dataSource: 'cluster0',
+				database: 'survey',
+				collection: 'survey',
+				filter: { _id: { $oid: params.surveyorId } }
+			})
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		return new Response(JSON.stringify(data), { status: 200 });
+	} catch (error) {
+		console.error('Error inserting data:', error);
+		return new Response('Error inserting data', { status: 500 });
+	}
+}
