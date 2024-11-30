7e2b1fe16390947152e4b5424373d94824cd046c
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-11-30 22:32:24 +0530

    Added vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			out: 'build'
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
